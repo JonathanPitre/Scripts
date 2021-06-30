6d53ae61c7a95177fc659b7b525a33c7a63918cd
Jonathan Pitre <jakeld@gmail.com>
2021-06-30 11:13:46 -0400

    Add Citrix Start-FileCaching script

--- Citrix/Start-FileCaching/README.md
+++ Citrix/Start-FileCaching/README.md
@@ -1,27 +1,22 @@
-# Reset-WEMCache
+# Start-FileCaching
 
 ## About
-Reset-WEMCache.ps1 is a powershell script to refresh Citrix Workspace Environment Agent cache.
-I took the original script from the article [CTX247927](https://support.citrix.com/article/CTX247927) and made it better.
+Start-FileCaching.ps1 is a powershell script to cache dll and exe files for non persistent environment like Citrix PVS/MCS IO.
+I took the original script from Citrix consulting and improved it.
 
-Reset-WEMCache.xml is a schedule task that will trigger the powershell script Reset-WEMCache.ps1 when Event ID 0 'Cache sync failed with error: SyncFailed' occurs.
+**Start-FileCaching.xml** is a schedule task that will trigger the powershell script Start-FileCaching.ps1 at machine startup.
 
 ## Supported Powershell Versions
 This has been tested with Powershell 5.1. Other versions may work but have not been tested, YMMV.
 
 ## Installation
-* Download the file Reset-WEMCache.ps1 and copy it to you're master image in the folder **C:\Scripts**.
-* Download the file Reset-WEMCache.xml and launch **Task Scheduler**.
+* Download the file Start-FileCaching.ps1 and copy it to you're master image in the folder **C:\Scripts**.
+* Download the file Start-FileCaching.xml and launch **Task Scheduler**.
 * Right-click on the **Task Scheduler Library** and click **Import**.
-* Select the file **Reset-WEMCache.xml** and press **OK**.
+* Select the file **Start-FileCaching.xml** and press **OK**.
 
 ## Usage
-Refresh the Citrix WEM Cache on the local computer:
+Cache dll and exe of you're local computer:
 ```powershell
-.\Reset-WEMCache.ps1
-```
-
-Refresh the Citrix WEM Cache on remote computer:
-```powershell
-Invoke-Command -ComputerName COMPUTER01 -FilePath C:\Scripts\Reset-WEMCache.ps1
+.\Start-FileCaching.ps1
 ```
---
+++ Citrix/Start-FileCaching/Start-FileCaching.ps1
@@ -0,0 +1,82 @@
+﻿# Get the current script directory
+Function Get-ScriptDirectory {
+    Remove-Variable appScriptDirectory
+    Try {
+        If ($psEditor) { Split-Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code Host
+        ElseIf ($psISE) { Split-Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE Host
+        ElseIf ($PSScriptRoot) { $PSScriptRoot } # Windows PowerShell 3.0-5.1
+        Else {
+            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            Exit 1
+        }
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+        Exit 2
+    }
+}
+
+# Function by Oliver Lipkau (https://devblogs.microsoft.com/scripting/use-powershell-to-work-with-any-ini-file)
+Function Get-IniContent ($filePath) {
+    $ini = @{}
+    switch -regex -file $FilePath
+    {
+        "^\[(.+)\]" # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+        "^(;.*)$" # Comment
+        {
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = "Comment" + $CommentCount
+            $ini[$section][$name] = $value
+        }
+        "(.+?)\s*=(.*)" # Key
+        {
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+    return $ini
+}
+
+# Cache files
+Function Start-FileCaching([string]$filename) {
+    Write-Host $filename
+    $Bytes = [System.IO.File]::ReadAllBytes($filename)
+}
+
+# Variables Declaration
+# Generic
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$env:SEE_MASK_NOZONECHECKS = 1
+$appScriptDirectory = Get-ScriptDirectory
+$Personality = "$env:SystemDrive\Personality.ini"
+$filelist = Get-ChildItem "$env:ProgramFiles\*.*", "${env:ProgramFiles(x86)}\*.*" -Recurse
+
+if ( Test-Path $Personality ) {
+    $iniContent = Get-IniContent $Personality;
+    $value = $iniContent["StringData"];
+}
+
+if ($value.values -eq "Shared") {
+    $sw = [Diagnostics.Stopwatch]::StartNew()
+
+    foreach ($f in $filelist) {
+        $ext = [IO.Path]::GetExtension($f)
+        Switch ($ext) {
+            {($_ -eq ".dll") -or ($_ -eq ".exe")} {Start-FileCaching($f)}
+        }
+    }
+
+    $sw.Stop()
+    $sw.Elapsed | Out-File -FilePath "$appScriptDirectory\CacheRunTime.log" -Force
+}
+else {
+    write-host "The disk is in Private mode!"
+    exit-pssession
+}
