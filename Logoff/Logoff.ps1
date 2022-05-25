7eeabc36465cc98783a21c41b5f5000ba0240734
Jonathan Pitre <jakeld@gmail.com>
2022-05-25 19:30:29 -0400

    Fix for FSLogix logoff

--- Logoff/Logoff.ps1
+++ Logoff/Logoff.ps1
@@ -7,6 +7,9 @@ $ErrorActionPreference = 'SilentlyContinue'
 # https://docs.microsoft.com/en-us/answers/questions/208676/onedrive-randomly-promting-to-removerestore-files.html?page=2&pageSize=10&sort=oldest
 If (Test-Path -Path "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe")
 {
+
+    # Breaks FSLogix logoff
+    <#
     If (Test-Path -Path "$env:ProgramFiles\WindowsPowershell\Modules\ODStatus\OneDriveLib.dll")
     {
         Import-Module -Name "$env:ProgramFiles\WindowsPowershell\Modules\ODStatus\OneDriveLib.dll"
@@ -23,17 +26,17 @@ If (Test-Path -Path "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe")
         Write-Host -Object "Microsoft OneDrive sync is completed." -ForegroundColor Green
 
         # Close OneDrive gracefully
-        Start-Process -FilePath "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe" -ArgumentList "/shutdown" -WindowStyle Hidden
+	    Start-Process -FilePath "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe" -ArgumentList "/shutdown" -WindowStyle Hidden
         Exit
     }
     Else
     {
         Write-Host -Object "Microsoft OneDrive Status Module is not currently installed!" -ForegroundColor Red
     }
-
+    #>
 
     # Close OneDrive gracefully
-    Start-Process -FilePath "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe" -ArgumentList "/shutdown" -WindowStyle Hidden
+	Start-Process -FilePath "$env:ProgramFiles\Microsoft OneDrive\OneDrive.exe" -ArgumentList "/shutdown" -WindowStyle Hidden
 }
 
 
@@ -51,6 +54,8 @@ Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\TokenBroker" -Recurse -Force
 Remove-Item -Path "HKCU:\Software\Microsoft\IdentityCRL" -Recurse -Force
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AAD" -Recurse -Force
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\WorkplaceJoin" -Recurse -Force
+
+Remove-Item -Path "$env:TEMP\*\ODNative.dll" -Recurse -Force
 
 # https://docs.microsoft.com/en-us/troubleshoot/windows-client/user-profiles-and-logon/slow-logon-with-blank-screen
 #Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\UFH\SHC" -Recurse -Force
